11deddad9871d677dfcf11e35db213b26849bfc8
Carriedbydog <113688545+Carriedbydog@users.noreply.github.com>
2023-09-23 14:37:06 +0000

    Deploying to gh-pages from @ Carriedbydog/goit-react-hw-05-movies@0fb65bbda9d32e15c82dd82f24aa792a7158f7cc 🚀

---
+++ static/js/803.d29a04ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[803],{803:function(e,t,r){r.r(t);r(791);var n=r(87),c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"Page not found"}),(0,c.jsx)(n.rU,{to:"/",children:"Back to Home"})]})})}}}]);
+//# sourceMappingURL=803.d29a04ad.chunk.js.map
